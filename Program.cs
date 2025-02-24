8f2c7e1c5e488e3c3d4da1fee53315efa2e18bb1
Beatriz Batista <byah24@outlook.com>
2025-02-24 12:40:18 -0300

    Finaliza programa da soma de dois números

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+int a= 0;
+int b= 0;
+int c= 0;
+
+Console.WriteLine("Insira um número:" );
+a=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira um número:" );
+b=int.Parse(Console.ReadLine());
+
+c=a+b;
+
+Console.WriteLine($"Seu resultado é:" + c);
